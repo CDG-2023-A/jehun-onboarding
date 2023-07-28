09b39ee9aeaed0018fc9da9e0908ab17abb0361b
JoJeHun <cho9759@gmail.com>
2023-07-28 14:21:24 +0900

    first commit

---
+++ src/main/java/com/example/jehunonboarding/JehunOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.example.jehunonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JehunOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JehunOnboardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jehunonboarding/JehunOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jehunonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JehunOnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
